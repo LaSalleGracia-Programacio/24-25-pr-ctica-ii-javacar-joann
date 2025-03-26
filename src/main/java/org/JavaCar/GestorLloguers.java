1afb482a5c2b35b64e08da95c955a8730a880601
Na.V <joan.navio@gracia.lasalle.cat>
2025-03-26 22:46:10 +0100

    Update GestorLloguers.java

--- src/main/java/org/JavaCar/GestorLloguers.java
+++ src/main/java/org/JavaCar/GestorLloguers.java
@@ -1,1 +1,10 @@
+package org.JavaCar;
 
+// Classe Estàtica GestorLloguers
+import java.util.List;
+
+public class GestorLloguers {
+    public static double calcularIngressosTotals(List<Vehicle> vehicles, int dies) {
+        return vehicles.stream().mapToDouble(v -> v.calcularPreu(dies)).sum();
+    }
+}
